92f8e7ebf65425dbcc8dfe113f1d1539c23d018e
ogundimu1 <abolajic@gmail.com>
2020-02-12 13:08:54 +0100

    Contact Store Using Laravel

---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+let axios = require('axios');
+
+Vue.component('contacts', require('./components/Contacts.vue').default);
+
+const app = new Vue({
+    el: '#app',
+});
+ 
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{csrf_token()}}">
+    <title>ContactStore</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+    <div class="container">
+        <div id="app">
+            <contacts></contacts>
+        </div>
+    </div>
+    <script src="/js/app.js"></script>
+</body> 
+</html>
